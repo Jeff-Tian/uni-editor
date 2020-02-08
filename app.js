fb668dedfd013cdcfb2370dd2c3a4353e242aa5b
Jeff Tian <jeff.tian@outlook.com>
2020-02-08 22:12:22 +0800

    feat: paste from office

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 
 ClassicEditor
     .create(document.querySelector('#editor'), {
-        plugins: [Essentials, Paragraph, Bold, Italic],
-        toolbar: ['bold', 'italic']
+        plugins: [Essentials, Paragraph, Bold, Italic, PasteFromOffice],
+        toolbar: ['bold', 'italic', 'heading', 'bulletedList', 'numberedList', 'fontFamily', 'undo', 'redo']
     })
     .then(editor => {
         console.log('Editor was initialized', editor);
